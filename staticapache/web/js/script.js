80634653b1881da14abb595694d19406d43c2fa7
Srinidhi Sivakumar <35161204+srinidhisivakumar@users.noreply.github.com>
2024-06-30 13:55:46 -0400

    Update script.js

--- staticapache/web/js/script.js
+++ staticapache/web/js/script.js
@@ -1,4 +1,4 @@
 function myFunction() {
- document.getElementById("demo").innerHTML = "Welcome to Contus";
+ document.getElementById("demo").innerHTML = "I'm a Sr. Azure DevOps Engineer- Expert - you can connect with me here https://www.linkedin.com/in/srinidhi-sivakumar-771a46116/";
 }
 
